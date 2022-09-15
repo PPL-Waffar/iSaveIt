01758e72242dd8174345be0c6b2a4dcdad4b313b
winaldoamadea <80420824+winaldoamadea@users.noreply.github.com>
2022-09-15 17:56:08 +0700

    Update settings.py

--- iSaveit/settings.py
+++ iSaveit/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-62s-ii-2vd@teampm58q7enk@rv^2z-79eht_6%5i6da(qn0=5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'iSaveit.urls'
