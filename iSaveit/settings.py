9b65b848f4fb8b13401cfa479611f7deeb103c56
winaldoamadea <80420824+winaldoamadea@users.noreply.github.com>
2022-09-15 17:55:07 +0700

    Update settings.py

--- iSaveit/settings.py
+++ iSaveit/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -50,6 +50,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'iSaveit.urls'
+
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 TEMPLATES = [
     {
