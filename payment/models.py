58b2c3ae21cbfd391438d258987cbc529c66ab49
winaldo.amadea <winaldoaa@gmail.com>
2022-09-21 23:33:52 +0700

    creating models and views for planned payment

--- payment/models.py
+++ payment/models.py
@@ -1,11 +1,23 @@
 from django.db import models
+from pocket.models import *
+PAYMENT_CHOICE = [
+    ('debit card', 'debit card'),
+    ('cash', 'cash'),
+    ('e-wallet', 'e-wallet'),
+]
 
 # Create your models here.
-class Account(models.Model):
+class Payment(models.Model):
 
-    name = models.CharField(primary_key=True, blank=True)
-    amount = models.BigIntegerField(default=False)
-    date = models.DateTimeField(auto_now=True)
+    pay_name = models.CharField(primary_key=True, blank=True)
+    pay_amount = models.BigIntegerField(default=False)
+    pay_date = models.DateTimeField(auto_now=True)
+    pay_categories = models.ForeignKey(Pocket, on_delete=models.SET_DEFAULT, default='NULL')
+    payment_choice = models.CharField(max_length=6, choices=PAYMENT_CHOICE, default='cash')
+    
+    
+
+
 
 
 
--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
-
+from django.views.decorators.csrf import csrf_exempt
+import json
+from importlib import import_module
+from django.conf import settings
+from .models import *
+import json
+from django.http.response import HttpResponse, HttpResponseForbidden, JsonResponse
 # Create your views here.
+@csrf_exempt
+def flutter_add_payment(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body.decode("utf-8"))
+            pay_name = data.get('input_payname')
+            pay_amount = data.get('input_payamount')
+            pay_date = data.get('input_paydate')
+            pay_categories = data.get('input_paycategories')
+            payment_choice = data.get('input_paymentchoice')
+            new_payment = Payment(pay_name = pay_name, pay_amount= pay_amount, pay_categories=pay_categories, payment_choice =payment_choice)
+
+            return JsonResponse({'isSuccessful':True},safe = False)
+        except Exception as E:
+            return JsonResponse({'isSuccesful':False,'error': str(E)},safe = True)
