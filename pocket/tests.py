102b7837fbef0293a6edc9387c05b16fd7873e07
Firlandi Ansyari <firlandi.althaf@ui.ac.id>
2022-09-26 22:26:24 +0700

    Created test for add_pocket

--- pocket/tests.py
+++ pocket/tests.py
@@ -1,3 +1,10 @@
+import json
 from django.test import TestCase
 
-# Create your tests here.
+class AddPocketTest(TestCase):
+    def test_add_pocket(self):
+        data = {'pocket_name': 'test_pocket', 'pocket_budget': 100}
+        response = self.client.post('/pocket/', json.dumps(data), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {'isSuccessful': True})
+
