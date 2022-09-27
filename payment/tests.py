b6d5e900a783fcd8d9979cb753fb7cfe61916f29
Firlandi Ansyari <firlandi.althaf@ui.ac.id>
2022-09-28 00:47:16 +0700

    [RED] Added test for update payment feature

--- payment/tests.py
+++ payment/tests.py
@@ -32,3 +32,27 @@ class GetPayment(TestCase):
                                     content_type="application/json")
         response = self.client.get('/payment/flu-get-payment/')
         self.assertEqual(response.status_code, 200)
+
+class UpdatePayment(TestCase):
+        def test_your_test(self):
+                python_dict = {
+                
+                        "input_payname": "test payment",
+                        "input_payamount": 10000,
+                        "input_paydate": '2022-10-25',
+                        "input_paymentchoice": 'cash'
+                }
+                response = self.client.post('/payment/flu-add-payment/',
+                                        json.dumps(python_dict),
+                                        content_type="application/json")
+                python_dict = {
+                
+                        "input_payname": "test payment",
+                        "input_payamount": 10000,
+                        "input_paydate": '2022-10-25',
+                        "input_paymentchoice": 'cash'
+                }
+                response = self.client.post('/payment/flu-update-payment/',
+                                        json.dumps(python_dict),
+                                        content_type="application/json")
+                self.assertEqual(response.status_code, 200)
