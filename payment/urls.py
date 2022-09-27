0bec9319f57b5a7a25920eb58bdf5b07bdc9dbb3
Firlandi Ansyari <firlandi.althaf@ui.ac.id>
2022-09-28 00:32:43 +0700

    [RED] Created the read payment feature

--- payment/urls.py
+++ payment/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from payment.views import flutter_add_payment
+from payment.views import flutter_add_payment, flutter_get_payment
 urlpatterns = [
      path('flu-add-payment/', flutter_add_payment),
+     path('flu-get-payment/', flutter_get_payment),
 ]
