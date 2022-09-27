4d66450d4deafd34f76b9383f92da037f68b2c8d
Firlandi Ansyari <firlandi.althaf@ui.ac.id>
2022-09-28 00:32:54 +0700

    [RED] Add tests for read payment

--- payment/tests.py
+++ payment/tests.py
@@ -17,3 +17,18 @@ class AddPayment(TestCase):
                                     json.dumps(python_dict),
                                     content_type="application/json")
         self.assertEqual(response.status_code, 200)
+
+class GetPayment(TestCase):
+    def test_your_test(self):
+        python_dict = {
+            
+                "input_payname": "test payment",
+                "input_payamount": 10000,
+                "input_paydate": '2022-10-25',
+                "input_paymentchoice": 'cash'
+        }
+        response = self.client.post('/payment/flu-add-payment/',
+                                    json.dumps(python_dict),
+                                    content_type="application/json")
+        response = self.client.get('/payment/flu-get-payment/')
+        self.assertEqual(response.status_code, 200)
