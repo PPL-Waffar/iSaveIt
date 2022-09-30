722ffe08f9eb2a96870ae484c79af5b8920545d0
Firlandi Ansyari <firlandi.althaf@ui.ac.id>
2022-09-30 21:38:02 +0700

    [GREEN] Added tests for view payment

--- payment/tests.py
+++ payment/tests.py
@@ -27,16 +27,40 @@ class AddPayment(TestCase):
         self.assertEqual(response.status_code,200)
 
 class GetPayment(TestCase):
-    def test_your_test(self):
-        python_dict = {
-            
-                "input_payname": "test payment",
-                "input_payamount": 10000,
-                "input_paydate": '2022-10-25',
-                "input_paymentchoice": 'cash'
-        }
-        response = self.client.post('/payment/flu-add-payment/',
-                                    json.dumps(python_dict),
-                                    content_type="application/json")
-        response = self.client.get('/payment/flu-get-payment/')
-        self.assertEqual(response.status_code, 200)
+    def setup_account(self):
+        self.client.post('/user/flu-register-user/',json.dumps({
+            'email' : 'test@test.com',
+            'name' : 'testwithdjango',
+            'password': 'test',
+        }),content_type='application/json')
+    
+    #create a test to get payment
+    def test_getpayment(self):
+        self.setup_account()
+        session = self.client.session
+        session['_auth_user_id'] = 'test@test.com'
+        session.save()
+
+        response = self.client.post('/payment/flu-add-payment/',json.dumps({
+            'session_id': session.session_key,
+            'input_payname': 'test',
+            'input_payamount': '100',
+            'input_paydate': '2020-01-01',
+            'input_paymentchoice': 'cash'
+        }),content_type='application/json')
+
+        response = self.client.post('/payment/flu-add-payment/',json.dumps({
+            'session_id': session.session_key,
+            'input_payname': 'test2',
+            'input_payamount': '1002',
+            'input_paydate': '2022-01-01',
+            'input_paymentchoice': 'cash'
+        }),content_type='application/json')
+
+        response = self.client.get('/payment/flu-get-payment/',{
+            'session_id': session.session_key,
+        })
+
+        print(response.content)
+
+        self.assertEqual(response.status_code,200)
