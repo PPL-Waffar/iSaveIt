01639476d7f0bc5c9f0218b553b97268a2c15cfa
Firlandi Ansyari <firlandi.althaf@ui.ac.id>
2022-10-01 21:30:55 +0700

    [GREEN] Added tests for add pocket

--- pocket/tests.py
+++ pocket/tests.py
@@ -1,10 +1,23 @@
 import json
 from django.test import TestCase
-
 class AddPocketTest(TestCase):
-    def test_add_pocket(self):
-        data = {'pocket_name': 'test_pocket', 'pocket_budget': 100}
-        response = self.client.post('/pocket/', json.dumps(data), content_type='application/json')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json(), {'isSuccessful': True})
+    def setup_account(self):
+        self.client.post('/user/flu-register-user/',json.dumps({
+            'email' : 'test@test.com',
+            'name' : 'testwithdjango',
+            'password': 'test',
+        }),content_type='application/json')
 
+    def test_addpocket(self):
+        self.setup_account()
+        session = self.client.session
+        session['_auth_user_id'] = 'test@test.com'
+        session.save()
+
+        response = self.client.post('/pocket/add-pocket/',json.dumps({
+            'session_id' : session.session_key,
+            'input_pocketname' : 'testpocket',
+            'input_pocketbudget' : 1000,
+        }),content_type='application/json')
+
+        self.assertEqual(response.status_code, 200)
