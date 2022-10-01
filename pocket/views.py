308f6ef09dabe9dbb1c4b9686c32cebe1c79fd8c
Firlandi Ansyari <firlandi.althaf@ui.ac.id>
2022-10-01 21:09:40 +0700

    [RED] Connected add pocket to user models

--- pocket/views.py
+++ pocket/views.py
@@ -1,16 +1,26 @@
+from importlib import import_module
 import json
+
+from user.models import Account
 from .models import Pocket
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
+from django.conf import settings
 
 @require_http_methods(["POST"])
 @csrf_exempt
 def add_pocket(request):
     if request.method == "POST":
-        data = json.loads(request.body.decode('utf-8'))
-        pocket_name = data['pocket_name']
-        pocket_budget = data['pocket_budget']
-        new_pocket = Pocket(pocket_name=pocket_name, pocket_budget=pocket_budget)
+        data = json.loads(request.body.decode("utf-8"))
+        session_id = data.get('session_id')
+        engine = import_module(settings.SESSION_ENGINE)
+        sessionstore = engine.SessionStore
+        session = sessionstore(session_id)
+        email = session.get('_auth_user_id')
+        pocket_name = data.get('input_pocketname')
+        pocket_budget = data.get('input_pocketbudget')
+        owninguser = Account.objects.get(email = email)
+        new_pocket = Pocket(user_pocket = owninguser ,pocket_name = pocket_name, pocket_budget = pocket_budget)
         new_pocket.save()
     return JsonResponse({'isSuccessful':True},safe = False)
