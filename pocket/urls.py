6a5674075ba55c9271beaf8d8a103333feb75307
raihan.fadhila01 <raihan.fadhila01@ui.ac.id>
2022-10-02 11:54:06 +0700

    [REFACTOR] Fixed urls for delete pocket and edit pocket

--- pocket/urls.py
+++ pocket/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import add_pocket, delete_pocket, edit_pocket
 
 urlpatterns = [
-    path('', add_pocket, name='add_pocket'),
-    path('delete/', delete_pocket, name='delete_pocket'),
-    path('edit/', edit_pocket, name='edit_pocket'),
+    path('add-pocket', add_pocket, name='add_pocket'),
+    path('delete-pocket/', delete_pocket, name='delete_pocket'),
+    path('edit-pocket/', edit_pocket, name='edit_pocket'),
 ]
