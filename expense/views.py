1c67dc229cabc66471b7ad4c1c0b3786b9fb41bf
winaldo.amadea <winaldoaa@gmail.com>
2022-10-09 00:51:21 +0700

    [RED] Add expense

--- expense/views.py
+++ expense/views.py
@@ -3,6 +3,8 @@ from django.views.decorators.csrf import csrf_exempt
 from importlib import import_module
 from django.conf import settings
 from expense.models import Expense
+from pocket.models import Pocket
+from payment.models import Payment
 from user.models import *
 import json
 from django.http.response import  JsonResponse
@@ -31,3 +33,25 @@ def view_expense(request):
                 "expense_pocket" : expense.expense_pocket,
             })
         return JsonResponse(expense_list,safe = False)
+
+@require_http_methods(["POST"])
+@csrf_exempt
+#Create add expense
+def add_expense(request):
+    if request.method == 'POST':
+        session_id = request.POST.get('session_id')
+        engine = import_module(settings.SESSION_ENGINE)
+        sessionstore = engine.SessionStore
+        session = sessionstore(session_id)
+        email = session.get('_auth_user_id')
+        owninguser = Account.objects.get(email = email)
+        expense_name = request.POST.get('expense_name')
+        expense_amount = request.POST.get('expense_amount')
+        expense_date = request.POST.get('expense_date')
+        expense_type = request.POST.get('expense_type')
+        expense_person = request.POST.get('expense_person')
+        expense_payment_choice = request.POST.get('expense_payment_choice')
+        expense_pocket = request.POST.get('expense_pocket')
+        expense = Expense(user_expense = owninguser, expense_name = expense_name, expense_amount = expense_amount, expense_date = expense_date, expense_type = expense_type, expense_person = expense_person, expense_payment_choice = expense_payment_choice, expense_pocket = expense_pocket)
+        expense.save()
+        return JsonResponse({"status" : "success"})
