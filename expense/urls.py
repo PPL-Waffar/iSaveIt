5237935abfe00ecde97638a34aec5e6c5a47c953
Firlandi Ansyari <firlandi.althaf@ui.ac.id>
2022-10-22 20:48:21 +0700

    [GREEN] add tests for total expense

--- expense/urls.py
+++ expense/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import add_expense, view_expense
+from .views import add_expense, total_expense, view_expense
 
 urlpatterns = [
      path('view-expense/', view_expense),
      path('add-expense/', add_expense),
+     path('total-expense/', total_expense),
 ]
