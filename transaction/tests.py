36f25d7bfe86bb792b95e558eebb5da6b0ee5a45
winaldo.amadea <winaldoaa@gmail.com>
2022-10-25 12:57:26 +0700

    [GREEN] Fix test and url

--- transaction/tests.py
+++ transaction/tests.py
@@ -1,8 +1,7 @@
+import datetime
 from django.test import TestCase
 import json
-
-from transaction.models import Transaction
-
+from json import JSONEncoder
 class TransactionTest(TestCase):
     def setup_account(self):
         self.client.post('/user/flu-register-user/',json.dumps({
@@ -12,6 +11,7 @@ class TransactionTest(TestCase):
         }),content_type='application/json')
 
     def test_input_transaction(self):
+        
         self.setup_account()
         session = self.client.session
         session['_auth_user_id'] = 'test@test.com'
@@ -34,7 +34,7 @@ class TransactionTest(TestCase):
         }),content_type='application/json')
         self.assertEqual(response.status_code, 200)
 
-    def test_get_transaction(self):
+    def test_gettransaction(self):
         self.setup_account()
         session = self.client.session
         session['_auth_user_id'] = 'test@test.com'
@@ -45,7 +45,6 @@ class TransactionTest(TestCase):
             'input_pocketname' : 'testpocket',
             'input_pocketbudget' : 1000,
         }),content_type='application/json')
-
         response = self.client.post('/transaction/input-transaction/',json.dumps({
             'session_id' : session.session_key,
             'input_transaction_payment_name' : 'warteg',
@@ -57,8 +56,12 @@ class TransactionTest(TestCase):
         }),content_type='application/json')
 
         response = self.client.get('/transaction/get-transaction/',{
-            'session_id' : session.session_key,
+            'session_id': session.session_key,
         })
         self.assertEqual(response.status_code, 200)
+
+
+
+
         
 
