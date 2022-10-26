8f96b840b1a476febb9b59181015bae28f14d288
winaldo.amadea <winaldoaa@gmail.com>
2022-10-26 22:02:05 +0700

    [GREEN] Fix duplication code

--- transaction/tests.py
+++ transaction/tests.py
@@ -39,22 +39,6 @@ class TransactionTest(TestCase):
         session = self.client.session
         session['_auth_user_id'] = 'test@test.com'
         session.save()
-
-        response = self.client.post('/pocket/add-pocket/',json.dumps({
-            'session_id' : session.session_key,
-            'input_pocketname' : 'testpocket',
-            'input_pocketbudget' : 1000,
-        }),content_type='application/json')
-        response = self.client.post('/transaction/input-transaction/',json.dumps({
-            'session_id' : session.session_key,
-            'input_transaction_payment_name' : 'warteg',
-            'input_transaction_amount' : 250,
-            'input_transaction_date' : '2023-05-11',
-            'input_transaction_transaction_type' : 'Expense',
-            'input_transaction_payment_type' : 'cash',
-            'input_transaction_pocket' : 'testpocket',
-        }),content_type='application/json')
-
         response = self.client.get('/transaction/get-transaction/',{
             'session_id': session.session_key,
         })
