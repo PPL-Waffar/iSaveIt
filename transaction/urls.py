f0831408a40dc39472ea39f3411eabb05a691fe5
winaldo.amadea <winaldoaa@gmail.com>
2022-10-31 18:47:26 +0700

    changes for backend

--- transaction/urls.py
+++ transaction/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import input_transaction,get_transaction
+from .views import input_transaction, get_transaction,view_all_transaction
 
 urlpatterns = [
     path('input-transaction/', input_transaction),
     path('get-transaction/', get_transaction),
+    path('view-transaction/', view_all_transaction),
     
 ]
