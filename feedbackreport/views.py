23fe0b747fe0f71280a719c7f095a087a935a38d
raihan.fadhila01 <raihan.fadhila01@ui.ac.id>
2022-11-02 22:23:06 +0700

    [RED] Create Delete Feedback Report Feature

--- feedbackreport/views.py
+++ feedbackreport/views.py
@@ -24,3 +24,19 @@ def add_feedback_report(request):
         new_feedback_report = Feedback(user_feedback = owninguser, feedback_title = feedback_title, feedback_feature = feedback_feature, feedback_textbox = feedback_textbox)
         new_feedback_report.save()
     return JsonResponse({'isSuccessful':True},safe = False)
+
+@require_http_methods(["DELETE"])
+@csrf_exempt
+def delete_feedback_report(request):
+    if request.method == "DELETE":
+        data = json.loads(request.body.decode("utf-8"))
+        session_id = data.get('session_id')
+        engine = import_module(settings.SESSION_ENGINE)
+        sessionstore = engine.SessionStore
+        session = sessionstore(session_id)
+        email = session.get('_auth_user_id')
+        feedback_title = data.get('input_feedback_title')
+        owninguser = Account.objects.get(email = email)
+        feedback_report = Feedback.objects.get(user_pocket = owninguser, feedback_title = feedback_title)
+        feedback_report.delete()
+    return JsonResponse({'isSuccessful':True},safe = False)
