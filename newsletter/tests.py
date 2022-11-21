e9b89f01a0797591b3b557a65129a179d694d3f4
firlandiansyari <firlandi.althaf@ui.ac.id>
2022-11-22 02:02:40 +0700

    [CHORES] Fixed code smells

--- newsletter/tests.py
+++ newsletter/tests.py
@@ -4,21 +4,21 @@ from newsletter.views import add_newsletter
 
 class AddNewsletterTest(TestCase):
     def test_add_newsletter(self):
-        file = SimpleUploadedFile(name='ui_logo.jpg', content=open('newsletter/pictures/ui_logo.jpg', 'rb').read(), content_type='image/jpeg')
+        test_image = SimpleUploadedFile(name='ui_logo.jpg', content=open('newsletter/pictures/ui_logo.jpg', 'rb').read(), content_type='image/jpeg')
         response = add_newsletter((self.client.post(
             '/add-newsletter/', {
                 'newsletter_text': 'test',
-                'newsletter_picture': file,
+                'newsletter_picture': test_image,
                 'newsletter_category': 'tips'
                 })).wsgi_request)
         self.assertEqual(response.status_code, 200)
 
     def test_add_newsletter_negative(self):
-        file = SimpleUploadedFile(name='ui_logo.jpg', content=open('newsletter/pictures/ui_logo.jpg', 'rb').read(), content_type='image/jpeg')
+        test_image = SimpleUploadedFile(name='ui_logo.jpg', content=open('newsletter/pictures/ui_logo.jpg', 'rb').read(), content_type='image/jpeg')
         response = add_newsletter((self.client.post(
             '/add-newsletter/', {
                 'newsletter_text': 'test',
-                'newsletter_picture': file,
+                'newsletter_picture': test_image,
                 'newsletter_category': 'tips'
                 })).wsgi_request)
         self.assertNotEqual(response.status_code, 404)
