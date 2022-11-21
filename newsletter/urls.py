c5531d15a92ef27dd8ba93cc3393a02c79ccd3ec
raihan.fadhila01 <raihan.fadhila01@ui.ac.id>
2022-11-21 14:55:30 +0700

    [RED] Create view list of newsletter

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from newsletter.views import add_newsletter
+from newsletter.views import add_newsletter, view_newsletter_list
 urlpatterns = [
-     path('add-newsletter/', add_newsletter,
-     )
+     path('add-newsletter/', add_newsletter),
+     path('view-newsletter-list/', view_newsletter_list),
 ]
--- newsletter/views.py
+++ newsletter/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from newsletter.forms import NewsletterForm
 from newsletter.models import Newsletter
+import json
+from django.http import HttpResponse, JsonResponse
 
 def index(request):
     return render(request, 'newsletter.html')
@@ -17,4 +19,16 @@ def add_newsletter(request):
         context = {}
         context['form'] = NewsletterForm()
         return render(request, 'newsletter.html', context)
+
+def view_newsletter_list(request):
+    all_newsletter = Newsletter.objects.all()
+    newsletter_list = []
+    for newsletter in all_newsletter:
+        newsletter_list.append({
+            'newsletter_text' : newsletter.newsletter_text,
+            'newsletter_picture' : newsletter.newsletter_picture,
+            'newsletter_category' : newsletter.newsletter_category,
+        })
+    data = json.dumps(newsletter_list)
+    return HttpResponse(data, content_type='application/json')
 
