2b6886cc90ed4cbd2cd9337aa87f253c9a08ab57
winaldo.amadea <winaldoaa@gmail.com>
2022-11-21 17:46:40 +0700

    [REFACTOR] clean code

--- transaction/tests.py
+++ transaction/tests.py
@@ -2,8 +2,8 @@ import datetime
 from django.test import TestCase
 import json
 from json import JSONEncoder
+myvar = '/transaction/input-transaction/'
 class TransactionTest(TestCase):
-
     
     def setup_account(self):
         self.client.post('/user/flu-register-user/',json.dumps({
@@ -14,7 +14,6 @@ class TransactionTest(TestCase):
     
 
     def test_input_transaction(self):
-        
         self.setup_account()
         session = self.client.session
         session['_auth_user_id'] = 'test@test.com'
@@ -26,7 +25,7 @@ class TransactionTest(TestCase):
             'input_pocketbudget' : 1000,
         }),content_type='application/json')
 
-        response = self.client.post('/transaction/input-transaction/',json.dumps({
+        response = self.client.post(myvar,json.dumps({
             'session_id' : session.session_key,
             'input_transaction_payment_name' : 'warteg',
             'input_transaction_amount' : 250,
@@ -57,18 +56,14 @@ class TransactionTest(TestCase):
         })
         self.assertEqual(response.status_code, 404)
     
-    def test_delete_transaction_wrong_pocket(self):
-        self.setup_account()
-        session = self.client.session
-        session['_auth_user_id'] = 'test@test.com'
-        session.save()
-        response = self.client.post('/pocket/add-pocket/',json.dumps({
+    def input_trans(self,session):
+        self.client.post('/pocket/add-pocket/',json.dumps({
             'session_id' : session.session_key,
             'input_pocketname' : 'testpocket',
             'input_pocketbudget' : 1000,
         }),content_type='application/json')
 
-        response = self.client.post('/transaction/input-transaction/',json.dumps({
+        self.client.post('/transaction/input-transaction/',json.dumps({
             'session_id' : session.session_key,
             'input_transaction_payment_name' : 'warteg',
             'input_transaction_amount' : 250,
@@ -77,7 +72,12 @@ class TransactionTest(TestCase):
             'input_transaction_payment_type' : 'cash',
             'input_transaction_pocket' : 'testpocket',
         }),content_type='application/json')
-        
+    def test_delete_transaction_wrong_pocket(self):
+        self.setup_account()
+        session = self.client.session
+        session['_auth_user_id'] = 'test@test.com'
+        session.save()
+        self.input_trans(session)
         response = self.client.post('/transaction/delete-transaction/',json.dumps({
             'session_id' : session.session_key,
             'transaction_payment_name' : 'warteg',
@@ -90,22 +90,7 @@ class TransactionTest(TestCase):
         session = self.client.session
         session['_auth_user_id'] = 'test@test.com'
         session.save()
-        response = self.client.post('/pocket/add-pocket/',json.dumps({
-            'session_id' : session.session_key,
-            'input_pocketname' : 'testpocket',
-            'input_pocketbudget' : 1000,
-        }),content_type='application/json')
-
-        response = self.client.post('/transaction/input-transaction/',json.dumps({
-            'session_id' : session.session_key,
-            'input_transaction_payment_name' : 'warteg',
-            'input_transaction_amount' : 250,
-            'input_transaction_date' : '2023-05-11',
-            'input_transaction_transaction_type' : 'Expense',
-            'input_transaction_payment_type' : 'cash',
-            'input_transaction_pocket' : 'testpocket',
-        }),content_type='application/json')
-
+        self.input_trans(session)
         response = self.client.post('/transaction/delete-transaction/',json.dumps({
             'session_id' : session.session_key,
             'transaction_payment_name' : 'warteg',
