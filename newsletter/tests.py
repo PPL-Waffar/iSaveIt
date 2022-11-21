a02bddd3220201b21638bbb8a524e2bc648cf357
firlandiansyari <firlandi.althaf@ui.ac.id>
2022-11-22 01:55:32 +0700

    [GREEN] FIxed tests for add newsletter

--- newsletter/tests.py
+++ newsletter/tests.py
@@ -1,17 +1,24 @@
-from audioop import reverse
 from django.test import TestCase
 from django.core.files.uploadedfile import SimpleUploadedFile
-
-from newsletter.forms import NewsletterForm
-from newsletter.models import Newsletter
 from newsletter.views import add_newsletter
 
 class AddNewsletterTest(TestCase):
     def test_add_newsletter(self):
         file = SimpleUploadedFile(name='ui_logo.jpg', content=open('newsletter/pictures/ui_logo.jpg', 'rb').read(), content_type='image/jpeg')
-        response = self.client.post('newsletter/add-newsletter/', {
-            'newsletter_text': 'test',
-            'newsletter_picture': file,
-            'newsletter_category': 'tips',
-        })
+        response = add_newsletter((self.client.post(
+            '/add-newsletter/', {
+                'newsletter_text': 'test',
+                'newsletter_picture': file,
+                'newsletter_category': 'tips'
+                })).wsgi_request)
+        self.assertEqual(response.status_code, 200)
 
+    def test_add_newsletter(self):
+        file = SimpleUploadedFile(name='ui_logo.jpg', content=open('newsletter/pictures/ui_logo.jpg', 'rb').read(), content_type='image/jpeg')
+        response = add_newsletter((self.client.post(
+            '/add-newsletter/', {
+                'newsletter_text': 'test',
+                'newsletter_picture': file,
+                'newsletter_category': 'tips'
+                })).wsgi_request)
+        self.assertNotEqual(response.status_code, 404)
