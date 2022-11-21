35f7976c42026787b74644f8f8e1508ebc43c80a
raihan.fadhila01 <raihan.fadhila01@ui.ac.id>
2022-11-21 15:10:12 +0700

    [GREEN] Create test for view list of newsletter

--- newsletter/tests.py
+++ newsletter/tests.py
@@ -14,4 +14,19 @@ class AddNewsletterTest(TestCase):
             'newsletter_picture': file,
             'newsletter_category': 'tips',
         })
+        
 
+    def test_get_list_newsletter(self):
+        
+        file = SimpleUploadedFile(name='ui_logo.jpg', content=open('newsletter/pictures/ui_logo.jpg', 'rb').read(), content_type='image/jpeg')
+        response = self.client.post('newsletter/add-newsletter/', {
+            'newsletter_text': 'test',
+            'newsletter_picture': file,
+            'newsletter_category': 'tips',
+        })
+        
+        response = self.client.get('/newsletter/view-newsletter-list/',{
+            'newsletter_text': 'test',
+        })
+    
+        self.assertEqual(response.status_code, 200)
