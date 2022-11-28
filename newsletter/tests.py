5446fa665a3c6e65e9c795140522a13578b6d3b9
firlandiansyari <firlandi.althaf@ui.ac.id>
2022-11-28 09:50:23 +0700

    [GREEN] Add tests for newsletter details

--- newsletter/tests.py
+++ newsletter/tests.py
@@ -1,6 +1,8 @@
+import json
 from django.test import TestCase
 from django.core.files.uploadedfile import SimpleUploadedFile
-from newsletter.views import add_newsletter
+from newsletter.models import Newsletter
+from newsletter.views import add_newsletter, view_detail_newsletter
 
 class AddNewsletterTest(TestCase):
     def test_add_newsletter(self):
@@ -22,3 +24,29 @@ class AddNewsletterTest(TestCase):
                 'newsletter_category': 'tips'
                 })).wsgi_request)
         self.assertNotEqual(response.status_code, 404)
+
+    def test_view_detail_newsletter(self):
+        test_image = SimpleUploadedFile(name='ui_logo.jpg', content=open('newsletter/pictures/ui_logo.jpg', 'rb').read(), content_type='image/jpeg')
+        add_newsletter((self.client.post(
+            '/add-newsletter/', {
+                'newsletter_text': 'test',
+                'newsletter_picture': test_image,
+                'newsletter_category': 'tips'
+                })).wsgi_request)
+        newsletter_id = Newsletter.objects.get(newsletter_text='test').id
+        response = view_detail_newsletter((self.client.get(
+            '/view-detail-newsletter/id=' + str(newsletter_id) + '/')).wsgi_request, newsletter_id)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_view_detail_newsletter_negative(self):
+        test_image = SimpleUploadedFile(name='ui_logo.jpg', content=open('newsletter/pictures/ui_logo.jpg', 'rb').read(), content_type='image/jpeg')
+        add_newsletter((self.client.post(
+            '/add-newsletter/', {
+                'newsletter_text': 'test',
+                'newsletter_picture': test_image,
+                'newsletter_category': 'tips'
+                })).wsgi_request)
+        newsletter_id = Newsletter.objects.get(newsletter_text='test').id
+        response = view_detail_newsletter((self.client.get(
+            '/view-detail-newsletter/id=' + str(newsletter_id) + '/')).wsgi_request, newsletter_id)
+        self.assertNotEqual(response.status_code, 200)
--- newsletter/urls.py
+++ newsletter/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from newsletter.views import add_newsletter
+from newsletter.views import add_newsletter, view_detail_newsletter
 urlpatterns = [
-     path('add-newsletter/', add_newsletter)
+     path('add-newsletter/', add_newsletter),
+     path('view-detail-newsletter/id=<int:id>/', view_detail_newsletter),
 ]
