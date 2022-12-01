737ea25578fe3dd5fad43fb9d98c3d20986ff984
winaldo.amadea <winaldoaa@gmail.com>
2022-12-02 03:59:05 +0700

    [RED] Edit newsletter

--- newsletter/views.py
+++ newsletter/views.py
@@ -54,3 +54,15 @@ def newsletterhtmk(request):
     obj=Newsletter.objects.all()
     return render(request,'list.html',{"obj":obj})
 
+def edit_newsletter(request):
+    if request.method == 'POST':
+        context = {}
+        context['form'] = NewsletterForm(request.POST, request.FILES)
+        if context['form'].is_valid():
+            context['form'].save()
+            context['form'] = NewsletterForm()
+            return render(request, 'newsletter.html', context)
+    else:
+        context = {}
+        context['form'] = NewsletterForm()
+        return render(request, 'newsletter.html', context)
