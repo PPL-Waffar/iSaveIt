9f3c5ac7b03525c8f7f8bba0b6cd64e39adce44a
firlandiansyari <firlandi.althaf@ui.ac.id>
2022-12-07 23:13:04 +0700

    [CHORES] Foxed code smells for newsletter

--- newsletter/tests.py
+++ newsletter/tests.py
@@ -6,13 +6,15 @@ from newsletter.views import add_newsletter, view_detail_newsletter
 
 image_name = 'ui_logo.jpg'
 image_path = 'newsletter/pictures/ui_logo.jpg'
+image_type = 'image/jpeg'
+add_newsletter_url = '/add-newsletter/'
 
 
 class AddNewsletterTest(TestCase):
     def test_add_newsletter(self):
-        test_image = SimpleUploadedFile(name='ui_logo.jpg', content=open('newsletter/pictures/ui_logo.jpg', 'rb').read(), content_type='image/jpeg')
+        test_image = SimpleUploadedFile(name=image_name, content=open(image_path, 'rb').read(), content_type=image_type)
         response = add_newsletter((self.client.post(
-            '/add-newsletter/', {
+            add_newsletter_url, {
                 'newsletter_text': 'test',
                 'newsletter_picture': test_image,
                 'newsletter_category': 'tips'
@@ -20,9 +22,9 @@ class AddNewsletterTest(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_add_newsletter_negative(self):
-        test_image = SimpleUploadedFile(name='ui_logo.jpg', content=open('newsletter/pictures/ui_logo.jpg', 'rb').read(), content_type='image/jpeg')
+        test_image = SimpleUploadedFile(name=image_name, content=open(image_path, 'rb').read(), content_type=image_type)
         response = add_newsletter((self.client.post(
-            '/add-newsletter/', {
+            add_newsletter_url, {
                 'newsletter_text': 'test',
                 'newsletter_picture': test_image,
                 'newsletter_category': 'tips'
@@ -30,9 +32,9 @@ class AddNewsletterTest(TestCase):
         self.assertNotEqual(response.status_code, 404)
 
     def test_view_detail_newsletter(self):
-        test_image = SimpleUploadedFile(name='ui_logo.jpg', content=open('newsletter/pictures/ui_logo.jpg', 'rb').read(), content_type='image/jpeg')
+        test_image = SimpleUploadedFile(name=image_name, content=open(image_path, 'rb').read(), content_type=image_type)
         add_newsletter((self.client.post(
-            '/add-newsletter/', {
+            add_newsletter_url, {
                 'newsletter_text': 'test',
                 'newsletter_picture': test_image,
                 'newsletter_category': 'tips'
@@ -43,9 +45,9 @@ class AddNewsletterTest(TestCase):
         self.assertEqual(response.status_code, 200)
     
     def test_view_detail_newsletter_negative(self):
-        test_image = SimpleUploadedFile(name='ui_logo.jpg', content=open('newsletter/pictures/ui_logo.jpg', 'rb').read(), content_type='image/jpeg')
+        test_image = SimpleUploadedFile(name=image_name, content=open(image_path, 'rb').read(), content_type=image_type)
         add_newsletter((self.client.post(
-            '/add-newsletter/', {
+            add_newsletter_url, {
                 'newsletter_text': 'test',
                 'newsletter_picture': test_image,
                 'newsletter_category': 'tips'
